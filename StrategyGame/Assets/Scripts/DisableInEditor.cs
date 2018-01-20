cf965d319fc24849329724f886201db08a0f907e
Aggrathon <antonbjo@gmail.com>
2018-01-20 16:37:11 +0200

    Use Unity Beta 2018.1

---
+++ StrategyGame/Assets/Scripts/DisableInEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableInEditor : MonoBehaviour {
+
+#if UNITY_EDITOR
+	private void Awake()
+	{
+		gameObject.SetActive(false);
+	}
+#endif
+}
